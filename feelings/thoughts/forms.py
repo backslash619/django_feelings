19a454543266faa782ef4e25860e5cfed0567c98
backslash619 <tarunkmr1234@gmail.com>
2017-10-15 14:26:46 +0530

    record form_

---
+++ feelings/thoughts/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+
+from . import models
+
+class ThoughtForm(forms.ModelForm):
+    class Meta:
+        fields = ("condition", "notes")
+        model = models.Thought
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
