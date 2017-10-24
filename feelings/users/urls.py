b524aba7232d60d0b0a8d4e700a1e14f256de91f
backslash619 <tarunkmr1234@gmail.com>
2017-10-24 15:56:11 +0530

    use external js library to have stretchy text field!!!!

--- feelings/users/urls.py
+++ feelings/users/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
+
 from . import views
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
     url(r'logout/$',views.logout_view,name='logout'),
     url(r'signup/$', views.SignupVoew.as_view(), name='signup'),
-    url(r'^dashboard/$', views.dashboard, name="dashboard"),
+    url(r'^dashboard/$', views.DashboardView.as_view(), name="dashboard"),
 ]
--- feelings/users/views.py
+++ feelings/users/views.py
@@ -1,19 +1,27 @@
+from braces.views import SelectRelatedMixin
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
-from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views import generic
 
 
-def dashboard(request):
-    return render(request, 'users/dashboard.html')
+class DashboardView(LoginRequiredMixin,
+                    generic.DeleteView,
+                    SelectRelatedMixin):
+    model = User
+    select_related = ('thoughts',)
+    template_name = "users/dashboard.html"
 
+    def get_object(self, queryset=None):
+        return self.request.user
 
 def logout_view(request):
     logout(request)
 
 
-class SignupVoew(CreateView):
+class SignupVoew(generic.CreateView):
     form_class = UserCreationForm
     template_name = "users/signup.html"
     success_url = reverse_lazy("home")
