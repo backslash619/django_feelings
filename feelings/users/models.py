10090e7b507e2fa645deead1f3f4d5b97bb4497c
backslash619 <tarunkmr1234@gmail.com>
2017-10-24 23:53:24 +0530

    build group model as families and companies!!!

--- feelings/users/models.py
+++ feelings/users/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Group(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    # just adds the class name for the the created by field
+    created_by = models.ForeignKey(User, related_name="%(class)s_created")
+    name = models.CharField(max_length=255)
+    description = models.TextField(default="", max_length=255)
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        abstract = True
+
+
+class Family(Group):
+    members = models.ManyToManyField(User, related_name="families")
+
+    class Meta:
+        verbose_name_plural = "Families"
+
+
+class Company(Group):
+    members = models.ManyToManyField(User, related_name="companies")
+
+    class Meta:
+        verbose_name_plural = "Companies"
